fc3b3bf110f4f25c16a4ea37fa975282f1e8fb26
Prashant Nema <Prashant.Nema@gcicom.net>
2017-03-02 12:41:25 +0000

    initial commit

---
+++ rating-file-processor/src/main/java/net/gcicom/rating/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.gcicom.rating;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RatingFileProcessorApplication.class);
+	}
+
+}
