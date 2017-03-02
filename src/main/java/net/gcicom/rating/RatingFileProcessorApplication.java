373b0e0173d349469d1649b28c3c9b67ecdd99a1
Prashant Nema <Prashant.Nema@gcicom.net>
2017-03-02 12:33:54 +0000

    Initial Commit

---
+++ src/main/java/net/gcicom/rating/RatingFileProcessorApplication.java
@@ -0,0 +1,12 @@
+package net.gcicom.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingFileProcessorApplication.class, args);
+	}
+}
