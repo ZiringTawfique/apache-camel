9ba0e706accdf3c1e3a680420ce066e74a066c68
Sathish Kumar <sathish.kumar@gcicom.net>
2017-04-24 16:33:57 +0100

    package refactoring

--- service-order-processor/src/main/java/net/gcicom/order/processor/entity/output/ChargeImportDtoToBillingReference.java
+++ service-order-processor/src/main/java/net/gcicom/order/processor/entity/output/ChargeImportDtoToBillingReference.java
@@ -27,6 +27,7 @@ import net.gcicom.domain.allspark.CustomerProductCharge;
 import net.gcicom.order.processor.entity.input.ChargeImportDto;
 import net.gcicom.order.processor.repository.BillingReferenceRepository;
 import net.gcicom.order.processor.service.GCIChargeImportService;
+import net.gcicom.order.processor.service.InvalidRecordException;
 import net.gcicom.order.processor.service.RecordAlreadyExistsException;
 import net.gcicom.order.processor.validator.BillingReferenceValidator;
 
@@ -49,7 +50,7 @@ public class ChargeImportDtoToBillingReference extends BaseEntity {
 
 	
 				
-			public List<BillingReference> convertToBillingReference(final List<ChargeImportDto> input, Exchange ex) throws ParseException, RecordAlreadyExistsException {
+			public List<BillingReference> convertToBillingReference(final List<ChargeImportDto> input, Exchange ex) throws ParseException, InvalidRecordException {
 				
 				List<BillingReference> cdrs = new ArrayList<>();
                   int loopCount=0; 
@@ -67,8 +68,9 @@ public class ChargeImportDtoToBillingReference extends BaseEntity {
 					//TODO get correct ID
 					
 					//ALl the field level  validation -NULL checks primarily 
-					BillingReferenceNullValidations(source);
-					BillingReferenceAllowedCharactersCheck(source);
+					BillingReferenceValidator.billingReferenceNullValidations(source);
+					BillingReferenceValidator.billingReferenceAllowedCharactersCheck(source);
+					BillingReferenceValidator.billingReferenceDateValidations(source);
 									
 					billingReference.setBillingReferenceID(111111L);					
 					billingReference.setOrderNumber(String.valueOf(source.getOrderNumber()));
@@ -161,8 +163,8 @@ public class ChargeImportDtoToBillingReference extends BaseEntity {
 						
 					boolean isValidBillingReference=	BillingReferenceValidator.billingReferenceValidation(result,source);
 					if(isValidBillingReference == false)
-						ex.setException(new RecordAlreadyExistsException("Record Already exists with this BIlling Reference",loopCount,ex)); 
-						throw new RecordAlreadyExistsException("Record Already exists with this BIlling Reference",loopCount,ex );
+						ex.setException(new InvalidRecordException("Record Already exists with this BIlling Reference",loopCount,ex)); 
+						throw new InvalidRecordException("Record Already exists with this BIlling Reference",loopCount,ex );
 					}
 				}
 				
@@ -185,113 +187,16 @@ public class ChargeImportDtoToBillingReference extends BaseEntity {
 			}
 			*/
 			
-			
-			public static void BillingReferenceNullValidations(ChargeImportDto source) throws RecordAlreadyExistsException{
-				
-				if(source.getActionCode() == null)
-					throw new RecordAlreadyExistsException("Fail - No Action Code" );
-				
-				if(source.getItemType() == null)
-					throw new RecordAlreadyExistsException("Fail - No Itemtype" );
-				
-				if(source.getCustomerName() == null)
-					throw new RecordAlreadyExistsException("Fail - No Customer Name" );
-				
-				if(source.getAccountNumber() == null)
-					throw new RecordAlreadyExistsException("Fail - No Account Number" );
-				
-				if(source.getNodeName() == null)
-					throw new RecordAlreadyExistsException("Fail - No Node Name" );
-				
-				if(source.getServiceCode() == null)
-				throw new RecordAlreadyExistsException("Fail - No Service Code" );
-				
-				if(source.getBillingReference() == null)
-					throw new RecordAlreadyExistsException("Fail - No Billing Reference " );
-				
-				if(source.getBillingReferenceDesc() == null)
-					throw new RecordAlreadyExistsException("Fail - No Billing Reference Description" );
-				
-				if(source.getCustomerServiceStartDate() == null)
-					throw new RecordAlreadyExistsException("Fail - No  Customer Service Start Date" );
-				
-			}
+	
 	
 			
 			
-			public static void BillingReferenceAllowedCharactersCheck (ChargeImportDto source) throws RecordAlreadyExistsException{
-				
-				if(!((StringUtils.isBlank(source.getAccountNumber()) && StringUtils.isEmpty(source.getAccountNumber())))  && ! Pattern.matches("^[A-Za-z0-9]+$", source.getAccountNumber()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Account Number" );
-				
-				if(!((StringUtils.isBlank(source.getNodeName()) && StringUtils.isEmpty(source.getNodeName())))  && ! Pattern.matches("^[A-Za-z0-9]+$", source.getNodeName()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Node Name" );				
-			       
-				if(!((StringUtils.isBlank(source.getServiceCode()) && StringUtils.isEmpty(source.getServiceCode())))  && ! Pattern.matches("^[A-Za-z0-9.._-]+$", source.getServiceCode()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Service Code" );
-			
-				
-				if(!((StringUtils.isBlank(source.getBillingReference()) && StringUtils.isEmpty(source.getBillingReference())))  && !  Pattern.matches("^[A-Za-z0-9..@_-]+$", source.getBillingReference()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Billing Reference" );
-				
-				if(!((StringUtils.isBlank(source.getBillingReferenceDesc()) && StringUtils.isEmpty(source.getBillingReferenceDesc())))  && !  Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getBillingReferenceDesc()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Billing Reference Description" );
-				
-				
-				if(!((StringUtils.isBlank(source.getGciSalesManager()) && StringUtils.isEmpty(source.getGciSalesManager())))  && !  Pattern.matches("^[A-Za-z.\\s_-]+$", source.getGciSalesManager()))
-					throw new RecordAlreadyExistsException("Fail - Invalid GCI Sales Manager" );
-								
-				if(!((StringUtils.isBlank(source.getCustomerSiteName()) && StringUtils.isEmpty(source.getCustomerSiteName())))  && ! Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getCustomerSiteName()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Billing Customer SiteName" );
-				
-				if(!((StringUtils.isBlank(source.getCustomerCustomReference()) && StringUtils.isEmpty(source.getCustomerCustomReference())))  && !  Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getCustomerCustomReference()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Customer Custom Reference" );
-				
-				if(!((StringUtils.isBlank(source.getCustomerCostCentre()) && StringUtils.isEmpty(source.getCustomerCostCentre())))  && !  Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getCustomerCostCentre()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Customer Customer Cost Centre" );
-				
-
-				if(!((StringUtils.isBlank(source.getCustomerPONumber()) && StringUtils.isEmpty(source.getCustomerPONumber())))  && !  Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getCustomerPONumber()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Customer PO Number" );
-			/*
-			 * Will be uncommented when implementing for charge import are added
-			 *  
-				if(!((StringUtils.isBlank(source.getInstallationPostCode()) && StringUtils.isEmpty(source.getInstallationPostCode())))  && !  Pattern.matches("^[A-Za-z.\\s]+$", source.getInstallationPostCode()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Installation PostCode" );
-									
-				if(!((StringUtils.isBlank(source.getSupplierOrderNumber()) && StringUtils.isEmpty(source.getSupplierOrderNumber())))  && !  Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getSupplierOrderNumber()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Supplier Order Number" );
-				
-				if(!((StringUtils.isBlank(source.getSupplierServiceReference()) && StringUtils.isEmpty(source.getSupplierServiceReference())))  && !  Pattern.matches("^[A-Za-z0-9.\\s).//@\\\\_(-£$]+$", source.getSupplierServiceReference()))
-					throw new RecordAlreadyExistsException("Fail - Invalid Supplier Service Reference" );
-					
-					*/
-			}
+		
 			
 			
 			
 			
 			
-			public static void BillingReferenceDateValidations(ChargeImportDto source) throws RecordAlreadyExistsException{
-				
-				if (source.getCustomerServiceStartDate().isBefore(source.getSupplierContractStartDate())	)	        		  
-						throw new RecordAlreadyExistsException("Fail - BillingReferenceStartDate cannot be before SupplierContractStartDate" );
-	
-				if (source.getCustomerContractStartDate().isBefore(source.getSupplierContractStartDate())	)	        		  
-					throw new RecordAlreadyExistsException("Fail - CustomerContractStartDate cannot be before SupplierContractStartDate" );
-	           
-			
-		      if (source.getSupplierContractEndDate().isBefore(source.getSupplierContractStartDate())	)	        		  
-				throw new RecordAlreadyExistsException("Fail - SupplierContractEndDate cannot be before SupplierContractStartDate" );
-		      
-		      if (source.getCustomerContractEndDate().isBefore(source.getCustomerContractStartDate())	)	        		  
-					throw new RecordAlreadyExistsException("Fail - CustomerContractEndDate cannot be before CustomerContractStartDate" );   
-		      
-              
-		      if (source.getCustomerContractStartDate().isBefore(source.getSupplierContractStartDate())	)	        		  
-					throw new RecordAlreadyExistsException("Fail - CustomerContractStartDate cannot be before SupplierContractStartDate" );
-		      
-            }
 			
 		
 
--- service-order-processor/src/main/java/net/gcicom/order/processor/service/ServiceOrderProcessorErrorHandler.java
+++ service-order-processor/src/main/java/net/gcicom/order/processor/service/ServiceOrderProcessorErrorHandler.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import net.gcicom.order.processor.entity.input.ChargeImportDto;
+import net.gcicom.order.processor.entity.output.ExcelFileHelper;
 
 import static net.gcicom.order.processor.common.AppConstants.TOTAL_RECORD_COUNT;
 import static net.gcicom.order.processor.service.ValidationTypes.RECORD_PROCESSING_ERROR;
@@ -61,8 +62,7 @@ public class ServiceOrderProcessorErrorHandler {
 		static Map<String, Object[]> excelHeader = new HashMap<String, Object[]>();
 		
 		
-		//CSV file header
-		private static final String FILE_HEADER = "Actioncode,ItemType,CustomerName,Account Number,NodeName,OrderNumber,ServiceCode,BillingReference,Description,EventTariffName,GCISalesManager,CustomerServiceStartDate,CustomerServiceEndDate,SupplierContractStartDate,SupplierContractEndDate,CustomerContractStartDate,CustomerContractEndDate,CustomerSiteName,CustomerCustomReference,CustomerCostCentre,CustomerPONumber,InstallationPostCode,SupplierOrderNumber,SupplierServiceReference,ProductCode,Description,CustomerReference,OrderNumber,Quantity,ChargeFrequency,UnitCostToGCI,UnitChargeToCustomer,TaxTypeFlag,ChargeStartDate,ChargeCeaseDate,ChargeBilledUntilDate,SupplierContractStartDate,SupplierContractEndDate,CustomerContractStartDate,CustomerContractEndDate,ChargeID";
+		
 		
 	private Logger logger = LoggerFactory.getLogger(ServiceOrderProcessorErrorHandler.class);
 	Map<Integer, String> data = new HashMap<>();
@@ -150,7 +150,7 @@ public void invalidRecord(Exchange e) {
 		
 			String  errorFileLocation= env.getProperty(ERROR_FILE_LOCATION);
 			 
-			writeExcelFile(chargeImportDtoList,errorFileLocation,fileName.toString());
+			ExcelFileHelper.writeExcelFile(chargeImportDtoList,errorFileLocation,fileName.toString());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -158,290 +158,7 @@ public void invalidRecord(Exchange e) {
 		//  writeCsvFile(data);
 		logger.error("Handled event has following error \n", data.size());
 	}
-	
-	
-	public static void writeCsvFile(Map data) {
-	
-		FileWriter fileWriter = null;				
-		try {
-			fileWriter = new FileWriter("NEW_ERRORED_FILE");
-
-			//Write the CSV file header
-			fileWriter.append(FILE_HEADER.toString());
-			
-			//Add a new line separator after the header
-			fileWriter.append(NEW_LINE_SEPARATOR);
-			
-			//Write a new student object list to the CSV file
-			Iterator iterator = data.keySet().iterator();
-		    while (iterator.hasNext()){
-		    	int key = (Integer) iterator.next();
-		        String occurrence = (String) data.get(key);
-		          fileWriter.append(occurrence);
-			}
-
-			
-			
-			System.out.println("CSV file was created successfully !!!");
-			
-		} catch (Exception e) {
-			System.out.println("Error in CsvFileWriter !!!");
-			e.printStackTrace();
-		} finally {
-			
-			try {
-				fileWriter.flush();
-				fileWriter.close();
-			} catch (IOException e) {
-				System.out.println("Error while flushing/closing fileWriter !!!");
-                e.printStackTrace();
-			}
-			
-		}
-	}
-	
-
-     		
-        	 public static void  writeExcelFile(List<ChargeImportDto> chargeImportDtoList,String errorFileLocation,String fileName) throws IOException {
-        		 
-        		 XSSFWorkbook  workbook = new XSSFWorkbook();
-        		 XSSFSheet  sheet = workbook.createSheet("Billing reference failed validation");
-        		
-        		
-        		    int rowCount = 0;
-        		    String excelFilePath = errorFileLocation+fileName;
-        		    Row row = sheet.createRow(++rowCount);
-        		  //  row = sheet.createRow(++rowCount);
-        		    writeHeader(row,sheet);
-        		    
-        		    for (ChargeImportDto chargeImportDto : chargeImportDtoList) {
-        		         writeBook(chargeImportDto, row);
-        		         row = sheet.createRow(++rowCount);
-        		    }
-        		 
-        		    try (FileOutputStream outputStream = new FileOutputStream(excelFilePath)) {
-        		        workbook.write(outputStream);
-        		    }
-        		}
-        	 
-        	 
-        	 private static void writeBook(ChargeImportDto aBook, Row row) {
-        		
-        		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-        		 LocalDateTime cellValue;
-        	        
-        	        
-     		       Cell  cell = row.createCell(0);
-        		    if(aBook.getActionCode() != null)
-        		    cell.setCellValue(aBook.getActionCode());
-        		 
-        		    cell = row.createCell(1);
-        		    if(aBook.getItemType() != null)
-        		    cell.setCellValue(aBook.getItemType());
-        		 
-        		    cell = row.createCell(2);
-        		    if(aBook.getCustomerName() != null)
-        		    cell.setCellValue(aBook.getCustomerName());
-        		    
-        		    cell = row.createCell(3);
-        		    if(aBook.getAccountNumber() != null)
-        		    cell.setCellValue(aBook.getAccountNumber());
-
-        		    cell = row.createCell(4);
-        		    if(aBook.getNodeName() != null)
-        		    cell.setCellValue(aBook.getNodeName());
-        		    
-        		    cell = row.createCell(5);
-        		    if(aBook.getOrderNumber() != null)
-        		    cell.setCellValue(aBook.getOrderNumber());
-        		    
-        		    cell = row.createCell(6);
-        		    if(aBook.getServiceCode() != null)
-        		    cell.setCellValue(aBook.getServiceCode());
-        		    
-        		    cell = row.createCell(7);
-        		    if(aBook.getBillingReference() != null)
-        		    cell.setCellValue(aBook.getBillingReference());
-        		    
-        		    cell = row.createCell(8);
-        		    if(aBook.getBillingReferenceDesc() != null)
-        		    cell.setCellValue(aBook.getBillingReferenceDesc());
-        		    
-        		    cell = row.createCell(9);
-        		    if(aBook.getEventTariffName() != null)
-        		    cell.setCellValue(aBook.getEventTariffName());
-        		    
-        		    
-        		    cell = row.createCell(10);
-        		    if(aBook.getGciSalesManager() != null)
-        		    cell.setCellValue(aBook.getGciSalesManager());
-        		    
-        		     cell = row.createCell(11);
-        		     if(aBook.getCustomerServiceStartDate() != null)
-        		     cell.setCellValue(aBook.getCustomerServiceStartDate().toString());
-        		   //   cellValue  = 	 LocalDateTime.parse(aBook.getCustomerServiceStartDate().toString());
-        		   //  cell.setCellValue(cellValue.format(formatter)); 
-        		     
-        		     
-        		    cell = row.createCell(12);
-        		    if(aBook.getCustomerServiceEndDate() != null)
-        		    cell.setCellValue(aBook.getCustomerServiceEndDate().format(formatter));
-        		    
-        		    cell = row.createCell(13);
-        		    if(aBook.getSupplierContractStartDate() != null)
-        		    cell.setCellValue(aBook.getSupplierContractStartDate().toString());
-
-        		    cell = row.createCell(14);
-        		    if(aBook.getSupplierContractEndDate() != null)
-        		    cell.setCellValue(aBook.getSupplierContractEndDate().toString());
-        		    
-        		    cell = row.createCell(15);
-        		    if(aBook.getCustomerContractStartDate() != null)
-        		    cell.setCellValue(aBook.getCustomerContractStartDate().toString());
-        		    
-        		    cell = row.createCell(16);
-        		    if(aBook.getCustomerContractEndDate() != null)
-        		    cell.setCellValue(aBook.getCustomerContractEndDate().toString());
-        		    
-        		    cell = row.createCell(17);
-        		    if(aBook.getCustomerSiteName() != null)
-        		    cell.setCellValue(aBook.getCustomerSiteName().toString());
-        		    
-        		    cell = row.createCell(18);
-        		    if(aBook.getCustomerCustomReference() != null)
-        		    cell.setCellValue(aBook.getCustomerCustomReference().toString());
-        		    
-        		    cell = row.createCell(19);
-        		    if(aBook.getCustomerCostCentre() != null)
-        		    cell.setCellValue(aBook.getCustomerCostCentre().toString());
-        		   
-        		    cell = row.createCell(20);
-        		    if(aBook.getCustomerPONumber() != null)
-        		    cell.setCellValue(aBook.getCustomerPONumber().toString());
-        		    
-        		    
-        		    cell = row.createCell(21);
-        		    if(aBook.getInstallationPostCode() != null)
-        		    cell.setCellValue(aBook.getInstallationPostCode().toString());
-        		    
-        		    
-        		    cell = row.createCell(22);
-        		    if(aBook.getSupplierOrderNumber() != null)
-        		    cell.setCellValue(aBook.getSupplierOrderNumber().toString());
-        		    
-        		    
-        		    cell = row.createCell(23);
-        		    if(aBook.getSupplierServiceReference() != null)
-        		    cell.setCellValue(aBook.getSupplierServiceReference().toString());
-        		    
-        		    
-        		    cell = row.createCell(24);
-        		    if(aBook.getProductCode() != null)
-        		    cell.setCellValue(aBook.getProductCode().toString());
-        		    
-        		    cell = row.createCell(25);
-        		    if(aBook.getDescription() != null)
-        		    cell.setCellValue(aBook.getDescription().toString());
-        		    
-        		   
-        		    cell = row.createCell(26);
-        		    if(aBook.getCustomerReference() != null)
-        		    cell.setCellValue(aBook.getCustomerReference().toString());
-        		    
-        		    cell = row.createCell(27);
-        		    if(aBook.getOrderNumber() != null)
-        		    cell.setCellValue(aBook.getOrderNumber().toString());
-        		    
-        		          		    
-        		    cell = row.createCell(28);
-        		    if(aBook.getQuantity() != null)
-        		    cell.setCellValue(aBook.getQuantity().toString());
-        		    
-        		    cell = row.createCell(29);
-        		    if(aBook.getChargeFrequency() != null)
-        		    cell.setCellValue(aBook.getChargeFrequency().toString());
-        		   
-        		    cell = row.createCell(30);
-        		    if(aBook.getUnitCostToGCI()!= null)
-        		    cell.setCellValue(aBook.getUnitCostToGCI().toString());
-        		    
-        		    
-        		    cell = row.createCell(31);
-        		    if(aBook.getUnitChargeToCustomer() != null)
-        		    cell.setCellValue(aBook.getUnitChargeToCustomer().toString());
-        		    
-        		    
-        		    cell = row.createCell(32);
-        		    if(aBook.getTaxTypeFlag() != null)
-        		    cell.setCellValue(aBook.getTaxTypeFlag().toString());
-        		    
-        		    
-        		    cell = row.createCell(33);
-        		    if(aBook.getChargeStartDate() != null)
-        		    cell.setCellValue(aBook.getChargeStartDate().toString());
-        		    
-        		    
-        		    cell = row.createCell(34);
-        		    if(aBook.getChargeCeaseDate() != null)
-        		    cell.setCellValue(aBook.getChargeCeaseDate().toString());
-        		    
-        		    cell = row.createCell(35);
-        		    if(aBook.getChargeBilledUntilDate() != null)
-        		    cell.setCellValue(aBook.getChargeBilledUntilDate().toString());
-        		    
-        		    
-        		    
-        		    cell = row.createCell(36);
-        		    if(aBook.getSupplierContractStartDate() != null)
-        		    cell.setCellValue(aBook.getSupplierContractStartDate().toString());
-        		    
-        		    cell = row.createCell(37);
-        		    if(aBook.getSupplierContractEndDate() != null)
-        		    cell.setCellValue(aBook.getSupplierContractEndDate().toString());
-        		    
-        		          		    
-        		    cell = row.createCell(38);
-        		    if(aBook.getCustomerContractStartDate() != null)
-        		    cell.setCellValue(aBook.getCustomerContractStartDate().toString());
-        		    
-        		    cell = row.createCell(39);
-        		    if(aBook.getCustomerContractEndDate() != null)
-        		    cell.setCellValue(aBook.getCustomerContractEndDate().toString());
-        		   
-        		    cell = row.createCell(40);
-        		    if(aBook.getChargeID()!= null)
-        		    cell.setCellValue(aBook.getChargeID().toString());
-        		    
-        		    
-        		    
-        		    cell = row.createCell(13);
-       		       if(aBook.getExceptionMessage() != null)
-       		       cell.setCellValue(aBook.getExceptionMessage().toString());
-        		}
-		
-		
-        	 
-			   private static  void writeHeader( Row row, XSSFSheet sheet) {
-	        		 Set<String> keyset = excelHeader.keySet();
-	        			int rownum = 0;
-	        			for (String key : keyset) {
-	        				 row = sheet.createRow(rownum++);
-	        				Object [] objArr = excelHeader.get(key);
-	        				int cellnum = 0;
-	        				for (Object obj : objArr) {
-	        					Cell cell = row.createCell(cellnum++);
-	        					if(obj instanceof Date) 
-	        						cell.setCellValue((Date)obj);
-	        					else if(obj instanceof Boolean)
-	        						cell.setCellValue((Boolean)obj);
-	        					else if(obj instanceof String)
-	        						cell.setCellValue((String)obj);
-	        					else if(obj instanceof Double)
-	        						cell.setCellValue((Double)obj);
-	        				}
-	        			}
-	   
-	     		}
+	 	 
         	
 		
 		
